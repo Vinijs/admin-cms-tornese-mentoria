0edb153b494cd3ef0c693ca1ef7c6bca1ff981f2
Vinijs <vini-spider-6@hotmail.com>
2024-01-12 18:02:52 -0300

    mudanças na API

---
+++ Controllers/API/AdministradoresController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using admin_cms.Models.Dominio.Entidades;
+using admin_cms.Models.Infraestrutura.Database;
+using admin_cms.Models.Infraestrutura.Autenticacao;
+
+namespace admin_cms.Controllers.API
+{
+    public class AdministradoresController : ControllerBase
+    {
+        private readonly ContextoCms _context;
+
+        public AdministradoresController(ContextoCms context)
+        {
+            _context = context;
+        }
+
+        // GET: Administradores
+        [HttpGet]
+        [Route("/api/administradores.json")]
+        public async Task<IActionResult> Index()
+        {
+              return StatusCode(200, await _context.Administradores.ToListAsync());
+        }
+
+    }
+}
