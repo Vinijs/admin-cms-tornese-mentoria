f1bb46dd50b57c3a080afc097af7fa2fa4c3a2ff
Vinijs <vini-spider-6@hotmail.com>
2024-01-26 16:55:07 -0300

    upload s3 amazon

--- Models/Dominio/Entidades/Administrador.cs
+++ Models/Dominio/Entidades/Administrador.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -27,6 +28,7 @@ namespace admin_cms.Models.Dominio.Entidades
         [Required]
         [MaxLength(150)]
         public string Senha { get; set; }
+        public string Imagem { get; set; }
 
         public string Acesso { get{ return "Admin"; } }
     }
